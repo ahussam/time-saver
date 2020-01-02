4a4e4981a728b44dde35e1a9e101e6b68ff828b1
Abdullah Hussam <me@ahussam.me>
2020-01-02 18:42:00 +0300

    first commit

---
+++ content.js
@@ -0,0 +1,28 @@
+//An array of unwanted to visit URLs 
+var blockedUrl = ['www.facebook.com',
+    'web.twitter.com',
+    'twitter.com',
+    'mobile.twitter.com',
+    'm.facebook.com',
+    'www.linkedin.com',
+    'touch.facebook.com',
+];
+//An array of wanted to visit URLs 
+var goodUrl = ['https://www.github.com',
+    'https://www.youtube.com/channel/UC9-y-6csu5WGm29I7JiwpnA',
+    'https://www.geeksforgeeks.org/',
+    'https://hackernoon.com/',
+    'https://github.com/Metnew/uxss-db#intro'
+];
+
+
+var url = window.location.host;
+
+for (let i = 0; i < blockedUrl.length; i++) {
+    if (url === blockedUrl[i]) {
+        randomNum = Math.floor(Math.random() * goodUrl.length)
+        var randomUrl = goodUrl[randomNum];
+        location = randomUrl;
+    }
+
+}
